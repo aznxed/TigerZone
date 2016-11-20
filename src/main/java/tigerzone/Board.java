a50dd01e066eebe0baccfdde97d7f6710dbd02a3
Spencer <Spencer@10.136.58.9>
2016-11-19 20:55:56 -0500

    Added Tile Num

--- src/main/java/tigerzone/Board.java
+++ src/main/java/tigerzone/Board.java
@@ -1,11 +1,11 @@
 package tigerzone;
-//Changes
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
 
+	public static int TILE_NUM = 77;
 	public static int MAX_ROWS = 143;
 	public static int MAX_COLS = 143;
 
