f6e1830af04d53d9d915e60aa34f948449c4dfce
Spencer <Spencer@slaptop>
2016-11-23 21:38:26 -0500

    More work on server protocol

--- src/main/java/tigerzone/tigerzoneServerProtocol.java
+++ src/main/java/tigerzone/tigerzoneServerProtocol.java
@@ -29,6 +29,8 @@ public class tigerzoneServerProtocol {
     private int matchNum = 1;
     private int roundNum = 1;
     private int roundTotal = 1;
+    private int moveTime = 1;
+    private boolean gameOver = false;
     
     private String tourPassword = "PersiaRocks!";
     private String playPassword = "Obiwan77";
@@ -73,9 +75,14 @@ public class tigerzoneServerProtocol {
         //Begin tournament
         //Begin Challenge
         else if (state == SentWelcome) {
-            theOutput = "NEW CHALLENGE " + challengeNum + " YOU WILL PLAY " + matchNum + " MATCH";
+        	if (matchNum > 1) {
+        		theOutput = "NEW CHALLENGE " + challengeNum + " YOU WILL PLAY " + matchNum + " MATCHES";
+        	}
+        	else {
+        		theOutput = "NEW CHALLENGE " + challengeNum + " YOU WILL PLAY " + matchNum + " MATCH";
+        	}
             state = SentNewChallenge;
-        } 
+        }
         //Begin Round
         else if (state == SentNewChallenge) {
             theOutput = "BEGIN ROUND " + roundNum + " OF " + roundTotal;
@@ -103,7 +110,98 @@ public class tigerzoneServerProtocol {
         	int tilesLeft = 6;
         	String tileString = "TLTTP LJTJ- JLJL- JJTJX LTTJB TLLT-";
             theOutput = "REMAINING " + tilesLeft + " TILES ARE [ " + tileString + " ]";
-            state = SentOpponent;
+            state = SentRemainingTiles;
+        }
+        //Send time till match starts
+        else if (state == SentRemainingTiles) {
+        	//TODO: Make non-constant
+        	int timeToMatch = 15;
+        	if (timeToMatch > 1){
+        		theOutput = "MATCH BEGINS IN " + timeToMatch + " SECONDS";
+        	}
+        	else {
+        		theOutput = "MATCH BEGINS IN " + timeToMatch + " SECOND";
+        	}
+            state = SentMatchBeginTime;
+        }
+        //Send move request
+        //TODO: Set timer for response
+        else if (state == SentMatchBeginTime) {
+        	//TODO: Make non-constant
+        	String gameID = "A";
+        	int moveNum = 1;
+        	String tile = "TLTTP";
+        	if (moveTime > 1){
+        		theOutput = "MAKE YOUR MOVE IN GAME " + gameID + " WITHIN " + moveTime + "SECONDS: MOVE " + moveNum + " PLACE " + tile;
+        	}
+        	else {
+        		theOutput = "MAKE YOUR MOVE IN GAME " + gameID + " WITHIN " + moveTime + "SECOND: MOVE " + moveNum + " PLACE " + tile;
+        	}
+            state = SentMakeAMove;
+        }
+        //Send Player Made a Move
+        else if (state == SentMakeAMove) {
+        	//TODO: Make non-constant
+        	int tilesLeft = 6;
+        	String gameID = "Z";
+        	String playerID = "BLARG";
+        	int moveNum = 0;
+        	theOutput = "GAME " + gameID + " MOVE " + moveNum + " PLAYER " + playerID + " ";
+        	if (true) {
+            	String[] split = theInput.split(" ");
+        		theOutput = theOutput + split[5] + " AT " + split[7] + " " + split[8] + " " + split[9] + " TIGER " + split[11];
+        	}
+        	//Illegal Tile Placement
+        	else if (true) {
+        	theOutput = theOutput + "FORFEITED: ILLEGAL TILE PLACEMENT";
+        	}
+        	//Illegal Meeple Placement
+        	else if (true) {
+        	theOutput = theOutput + "FORFEITED: ILLEGAL MEEPLE PLACEMENT";
+        	}
+        	//Invalid Meeple Placement
+        	else if (true) {
+        	theOutput = theOutput + "FORFEITED: ILLEGAL INVALID MEEPLE PLACEMENT";
+        	}
+        	//Timeout
+        	else if (true) {
+        	theOutput = theOutput + "FORFEITED: TIMEOUT";
+        	}
+        	//Illegal Message Received
+        	else if (true) {
+        	theOutput = theOutput + "FORFEITED: ILLEGAL MESSAGE RECEIVED";
+        	}
+        	if (!gameOver){
+        		state = SentMatchBeginTime;
+        	}
+        	else {
+        		state = SentMoveMade;
+        	}
+        }
+        else if (state == SentMoveMade) {
+        	//TODO: Remove Constants
+        	String gameID = "Z";
+        	String player1 = "player1";
+        	String player2 = "player2";
+        	int player1Score = -1;
+        	int player2Score = -2;
+        	theOutput = "GAME " + gameID + " OVER PLAYER " + player1 + " " + player1Score + " PLAYER " + player2 + " " + player2Score;
+            state = SentScore;
+        }
+        else if (state == SentScore) {
+        	//TODO: Remove Constants
+        	int round = 1;
+        	int roundTotal = 1;
+        	theOutput = "END OF ROUND " + round + " OF " + roundTotal;
+            state = SentEndRound;
+        }
+        else if (state == SentEndRound) {
+        	theOutput = "END OF CHALLENGES";
+        	state = SentEndChallenges;
+        }
+        else if (state == SentEndChallenges) {
+        	theOutput = "THANK YOU FOR PLAYING! GOODBYE";
+        	state = WAITING;
         }
         return theOutput;
     }
