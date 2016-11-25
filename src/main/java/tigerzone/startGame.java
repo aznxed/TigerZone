d17b10dfb8b2192ea780bdaaa1eed9e6df310d62
Spencer <Spencer@slaptop>
2016-11-25 00:32:30 -0500

    Cleanup

--- src/main/java/tigerzone/startGame.java
+++ src/main/java/tigerzone/startGame.java
@@ -8,26 +8,7 @@ public class startGame {
 	
 	public static void main(String[] args) {
 		
-		//Used for how many boards and AI that need to be made. 
-		int gameNum = 2;
-		
-		//Initialize deck
-		Tile tile1 = new Tile();
-		Tile tile2 = new Tile();
-		
-		tile1.setTopEdge(EdgeType.FIELD);
-		tile2.setTopEdge(EdgeType.FIELD);
-		
-		System.out.println(tile1.getTopEdge() == tile2.getTopEdge());
-		
-		//Initialize boards
-		Board board1 = new Board();
-		Board board2 = new Board();
-		
-		System.out.println(Board.CENTER_TILE);
-		
-		//Initialize Sockets
-		tcpClient tcpClient = new tcpClient();
+		//Nothing for the moment
 		
 	}
 }
