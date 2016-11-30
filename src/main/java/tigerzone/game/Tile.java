e14320b001b61162375952f934c7b5fd3ae5560a
Spencer <Spencer@10.136.62.235>
2016-11-29 19:01:46 -0500

    Fix incorrect tiles. END tiles should be changed into Lakes later.

--- src/main/java/tigerzone/game/Tile.java
+++ src/main/java/tigerzone/game/Tile.java
@@ -84,175 +84,175 @@ public class Tile {
 	{
 		switch(terrainType)
 		{
-			case"JJJJ-":	
-				TerrainType[] tileA = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-					TerrainType.JUNGLE };
-				return tileA;
-			case"JJJJX":	
-				TerrainType[] tileB = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+		case"JJJJ-":	
+			TerrainType[] tileA = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+				TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+				TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+				TerrainType.JUNGLE };
+			return tileA;
+		case"JJJJX":	
+			TerrainType[] tileB = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+				TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.DEN,
+				TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+				TerrainType.JUNGLE }; 
+			return tileB;
+		case"JJTJX":	
+			TerrainType[] tileC = { TerrainType.JUNGLE, TerrainType.JUNGLE,
 					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.DEN,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE };
+			return tileC;
+		case"TTTT-":	
+			TerrainType[] tileD = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
+					TerrainType.GAMETRAIL, TerrainType.JUNGLE,
+					TerrainType.GAMETRAIL, TerrainType.JUNGLE }; 
+			return tileD;
+		case"TJTJ-":	
+			TerrainType[] tileE = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE }; 
+			return tileE;
+		case"TJJT-":	
+			TerrainType[] tileF = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.GAMETRAIL, TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE }; 
+			return tileF;
+		case"TJTT-":	
+			TerrainType[] tileG = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE };//
+			return tileG;
+		case"LLLL-":	
+			TerrainType[] tileH = { TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE }; 
+			return tileH;	
+		case"JLLL-":	
+			TerrainType[] tileI = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.JUNGLE,
+					TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE };
+			return tileI;	
+		case"LLJJ-":	
+			TerrainType[] tileJ = { TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE }; 
+			return tileJ;	
+		case"JLJL-":	
+			TerrainType[] tileK = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE };
+			return tileK;
+		case"LJLJ-":	
+			TerrainType[] tileL = { TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.JUNGLE }; 
+			return tileL;
+		case"LJJJ-":	
+			TerrainType[] tileM = { TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
 					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
 					TerrainType.JUNGLE }; 
-				return tileB;
-			case"JJTJX":	
-				TerrainType[] tileC = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.DEN,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE };
-				return tileC;
-			case"TTTT-":	
-				TerrainType[] tileD = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
-						TerrainType.GAMETRAIL, TerrainType.JUNGLE,
-						TerrainType.GAMETRAIL, TerrainType.JUNGLE }; 
-				return tileD;
-			case"TJTJ-":	
-				TerrainType[] tileE = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE }; 
-				return tileE;
-			case"TJJT-":	
-				TerrainType[] tileF = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.GAMETRAIL, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE }; 
-				return tileF;
-			case"TJTT-":	
-				TerrainType[] tileG = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE };
-				return tileG;
-			case"LLLL-":	
-				TerrainType[] tileH = { TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE }; 
-				return tileH;	
-			case"JLLL-":	
-				TerrainType[] tileI = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE };
-				return tileI;	
-			case"LLJJ-":	
-				TerrainType[] tileJ = { TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE }; 
-				return tileJ;	
-			case"JLJL-":	
-				TerrainType[] tileK = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE };
-				return tileK;
-			case"LJLJ-":	
-				TerrainType[] tileL = { TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.JUNGLE }; 
-				return tileL;
-			case"LJJJ-":	
-				TerrainType[] tileM = { TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE }; 
-				return tileM;
-			case"JLLJ-":	
-				TerrainType[] tileN = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.JUNGLE };
-				return tileN;
-			case"TLJT-":	
-				TerrainType[] tileO = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE };
-				return tileO;
-			case"TLJTP":	
-				TerrainType[] tileP = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE };
-				return tileP;
-			case"JLTT-":	
-				TerrainType[] tileQ = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
-						TerrainType.GAMETRAIL, TerrainType.JUNGLE };
-				return tileQ;
-			case"JLTTB":	
-				TerrainType[] tileR = { TerrainType.JUNGLE, TerrainType.JUNGLE,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
-						TerrainType.GAMETRAIL, TerrainType.JUNGLE }; 
-				return tileR;
-			case"TLTJ-":	
-				TerrainType[] tileS = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE };
-				return tileS;
-			case"TLTJD":	
-				TerrainType[] tileT = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE };
-				return tileT;
-			case"TLLL-":	
-				TerrainType[] tileU = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.END,
-						TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE }; 
-				return tileU;
-			case"TLTT-":	
-				TerrainType[] tileV = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE }; 
-				return tileV;
-			case"TLTTP":	
-				TerrainType[] tileW = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE }; 
-				return tileW;
-			case"TLLT-":	
-				TerrainType[] tileX = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.LAKE }; 
-				return tileX;
-			case"TLLTB":	
-				TerrainType[] tileY = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.LAKE }; 
-				return tileY;
-			case"LJTJ-":	
-				TerrainType[] tileZ = { TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE }; 
-				return tileZ;
-			case"LJTJD":	
-				TerrainType[] tileZZ = { TerrainType.JUNGLE, TerrainType.LAKE,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.END,
-						TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE };
-				return tileZZ;
-			case"TLLLC":	
-				TerrainType[] tileZZZ = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-						TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
-						TerrainType.LAKE };
-				return tileZZZ;
-				}
+			return tileM;
+		case"JLLJ-":	
+			TerrainType[] tileN = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.JUNGLE };
+			return tileN;
+		case"TLJT-":	
+			TerrainType[] tileO = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE };
+			return tileO;
+		case"TLJTP":	
+			TerrainType[] tileP = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE };
+			return tileP;
+		case"JLTT-":	
+			TerrainType[] tileQ = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
+					TerrainType.GAMETRAIL, TerrainType.JUNGLE };
+			return tileQ;
+		case"JLTTB":	
+			TerrainType[] tileR = { TerrainType.JUNGLE, TerrainType.JUNGLE,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.GAMETRAIL, TerrainType.LAKE, TerrainType.JUNGLE,
+					TerrainType.GAMETRAIL, TerrainType.JUNGLE }; 
+			return tileR;
+		case"TLTJ-":	
+			TerrainType[] tileS = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE };
+			return tileS;
+		case"TLTJD":	
+			TerrainType[] tileT = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE };
+			return tileT;
+		case"TLLL-":	
+			TerrainType[] tileU = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.END,
+					TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE }; //TODO: END Should be changed to LAKE
+			return tileU;
+		case"TLTT-":	
+			TerrainType[] tileV = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE }; 
+			return tileV;
+		case"TLTTP":	
+			TerrainType[] tileW = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.END,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE }; 
+			return tileW;
+		case"TLLT-":	
+			TerrainType[] tileX = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.GAMETRAIL,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.LAKE }; 
+			return tileX;
+		case"TLLTB-":	
+			TerrainType[] tileY = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.GAMETRAIL,
+					TerrainType.LAKE, TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.LAKE }; 
+			return tileY;
+		case"LJTJ-":	
+			TerrainType[] tileZ = { TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.END,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE }; 
+			return tileZ;
+		case"LJTJD":	
+			TerrainType[] tileZZ = { TerrainType.JUNGLE, TerrainType.LAKE,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.END,
+					TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE };
+			return tileZZ;
+		case"TLLLC":	
+			TerrainType[] tileZZZ = { TerrainType.JUNGLE, TerrainType.GAMETRAIL,
+					TerrainType.JUNGLE, TerrainType.LAKE, TerrainType.END,
+					TerrainType.LAKE, TerrainType.LAKE, TerrainType.LAKE,
+					TerrainType.LAKE }; //END should be changed to Lake
+			return tileZZZ;
+			}
 		TerrainType[] empty = {}; 
 		return empty;
 	}
